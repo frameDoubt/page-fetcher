34e7aed810e804aa19bec469b9694ca8a12a35a9
Brandon Wright <brandon.w.wright4@gmail.com>
2021-01-15 07:31:40 +0000

    commented out console.log()'s

--- fetcher.js
+++ fetcher.js
@@ -3,10 +3,10 @@ const fs = require('fs');
 const userInput = process.argv.splice(2);
 
 request(userInput[0], (error, response, body) => {
-  console.log('Error report: ',error);
-  console.log('Status code: ', response.statusCode);
+  // console.log('Error report: ',error);
+  // console.log('Status code: ', response.statusCode);
   fs.writeFile(userInput[1], body, (err) => {
     if (err) {throw err};
-    console.log('File has successfully been downloaded');
+    console.log(`File has successfully been downloaded. It was ${Buffer.byteLength(body)} bytes`);
   });
 });
