be892fd05b5e7576a7180948de9c69db74ac5bc3
Brandon Wright <brandon.w.wright4@gmail.com>
2021-01-15 06:46:28 +0000

    trying to figure out how to implement buffer size call for my downloaded file

---
+++ fetcher.js
@@ -0,0 +1,12 @@
+const request = require('request');
+const fs = require('fs');
+const userInput = process.argv.splice(2);
+
+request(userInput[0], (error, response, body) => {
+  console.log('Error report: ',error);
+  console.log('Status code: ', response.statusCode);
+  fs.writeFile(userInput[1], body, (err) => {
+    if (err) {throw err};
+    console.log('File has successfully been downloaded');
+  });
+});
